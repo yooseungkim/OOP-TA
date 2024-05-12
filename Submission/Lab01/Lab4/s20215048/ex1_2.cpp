56bd73df7275f795d136ddc4436bcd6664501be5
yooseungkim <91931704+yooseungkim@users.noreply.github.com>
2024-05-12 13:33:12 +0000

    add test sets

---
+++ Submission/Lab01/Lab4/s20215048/ex1_2.cpp
@@ -0,0 +1,8 @@
+#include <iostream> 
+
+using namespace std; 
+
+int main() {
+    cout << "hello world2" << endl; 
+    return 0; 
+}
--- tool/test.sh
+++ tool/test.sh
@@ -1,6 +1,28 @@
+# $(...) : 결과 실행 후 값 대입
+# ls : 파일 리스트
+# wc -l : 개수 세기
+variable=$(find . -name '*.sh' | wc -l) 
+
+echo "$variable"
+
+# compile c++ file 
+g++ main.cpp -o main  
+
+# save into txt file 
+./main > main.txt
+
+# file input by line 
+while IFS= read -r line; do
+    echo "Text read from file: $line"
+done < main.txt
+
+output="" 
 for i in {1..10} 
 do
     g++ main.cpp -o main
-    echo "kafjdfklajklg" > test.csv
-    ./main > test.csv
+    output= ./main
 done 
+
+$output > test.csv
+cat test.csv
+
